60461df31755bd9161e02f969b80766658bebe2d
Vidura Nanayakkara <vidura_nanayakkara@hotmail.com>
2017-08-15 17:55:30 +0530

    Fix WSO2 IS restarts when puppet agent is enabled
    Resolves #20

--- manifests/params.pp
+++ manifests/params.pp
@@ -39,6 +39,7 @@ class wso2is::params {
     $packages                 = hiera_array('packages', undef)
     $template_list            = hiera_array('wso2::template_list')
     $file_list                = hiera_array('wso2::file_list', undef)
+    $file_list_copy_without_refresh    = hiera_array('wso2::file_list_copy_without_refresh', undef)
     $patch_list               = hiera('wso2::patch_list', undef)
     $system_file_list         = hiera_hash('wso2::system_file_list', undef)
     $directory_list           = hiera_array('wso2::directory_list', undef)
