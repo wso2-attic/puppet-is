afa272f55d335afc8ff38b8de0e72e89a89559a8
Ching Tien Shi <ctienshi@gmail.com>
2018-08-08 18:34:48 +0530

    Refractor startserver.pp

--- modules/wso2is/manifests/startserver.pp
+++ modules/wso2is/manifests/startserver.pp
@@ -1,16 +1,15 @@
 class wso2is::start{
 
-$install_path			 = '/usr/lib/wso2/wso2is/5.6.0'
+    $install_path			 = '/usr/lib/wso2/wso2is/5.6.0'
 
-exec { 'systemctl daemon-reload':
-    command  => "systemctl daemon-reload",
-    cwd      => "${install_path}/bin",
-    path     => '/usr/bin:/usr/sbin:/bin',
+    exec { 'systemctl daemon-reload':
+        command  => "systemctl daemon-reload",
+        cwd      => "${install_path}/bin",
+        path     => '/usr/bin:/usr/sbin:/bin',
     }
 
-service { 'wso2is':
-    enable 	 => true,
-    ensure 	 => running,
-}
-
+    service { 'wso2is':
+        enable 	 => true,
+        ensure 	 => running,
+    }
 }
