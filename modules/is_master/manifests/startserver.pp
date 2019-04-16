18e68d2a861275640c55b3e82e3d8978d01a66af
Savidude <mysticsavy@gmail.com>
2019-04-16 12:23:32 +0530

    Empty startserver manifest for IS Master

--- modules/is_master/manifests/startserver.pp
+++ modules/is_master/manifests/startserver.pp
@@ -1,13 +1,4 @@
 class is_master::startserver inherits is_master::params {
 
-  exec { 'daemon-reload':
-    command     => "systemctl daemon-reload",
-    path        => "/usr/bin/",
-    refreshonly => true,
-  }
 
-  # service { $service_name:
-  #   enable => true,
-  #   ensure => running,
-  # }
 }
