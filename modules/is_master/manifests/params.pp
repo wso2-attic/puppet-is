962ac0bfee266cea5575a9f2ff5210350ab1d565
Savidude <mysticsavy@gmail.com>
2019-04-16 09:49:31 +0530

    Create module for IS Master node

---
+++ modules/is_master/manifests/params.pp
@@ -0,0 +1,70 @@
+#----------------------------------------------------------------------------
+#  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#----------------------------------------------------------------------------
+
+class is_master::params {
+
+  $user = 'wso2carbon'
+  $user_group = 'wso2'
+  $product = 'wso2is'
+  $product_version = '5.7.0'
+  $service_name = 'wso2is'
+
+  # Define the templates
+  $start_script_template = 'bin/wso2server.sh'
+
+  $template_list = [
+    'repository/conf/carbon.xml',
+    'repository/conf/user-mgt.xml',
+    # 'repository/conf/datasources/master-datasources.xml',
+    # 'repository/conf/axis2/axis2.xml',
+    #	'repository/conf/identity/identity.xml',
+  ]
+
+  # carbon.xml configs
+  $ports_offset = 0
+
+  $security_keystore_location = '${carbon.home}/repository/resources/security/wso2carbon.jks'
+  $security_keystore_type = 'JKS'
+  $security_keystore_password = 'wso2carbon'
+  $security_keystore_key_alias = 'wso2carbon'
+  $security_keystore_key_password = 'wso2carbon'
+
+  $security_internal_keystore_location = '${carbon.home}/repository/resources/security/wso2carbon.jks'
+  $security_internal_keystore_type = 'JKS'
+  $security_internal_keystore_password = 'wso2carbon'
+  $security_internal_keystore_key_alias = 'wso2carbon'
+  $security_internal_keystore_key_password = 'wso2carbon'
+
+  $security_trust_store_location = '${carbon.home}/repository/resources/security/client-truststore.jks'
+  $security_trust_store_type = 'JKS'
+  $security_trust_store_password = 'wso2carbon'
+
+  # user-mgt.xml
+  $admin_username = 'admin'
+  $admin_password = 'admin'
+
+  # Directories
+  $products_dir = "/usr/local/wso2"
+  $puppet_env = "/etc/puppet/code/environments/production"
+
+  # Product and installation paths
+  $product_binary = "${product}-${product_version}.zip"
+  $distribution_path = "${products_dir}/${product}/${product_version}"
+  $install_path = "${distribution_path}/${product}-${product_version}"
+
+  # Java path
+  $java_home = "$install_path/corretto"
+}
