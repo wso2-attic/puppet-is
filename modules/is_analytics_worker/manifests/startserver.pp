d9be8e8150e7a34452039b5a9e99b056cf99aabb
Savidude <mysticsavy@gmail.com>
2019-06-05 09:13:09 +0530

    Move jdk and wso2 distribution resources into the common module

--- modules/is_analytics_worker/manifests/startserver.pp
+++ modules/is_analytics_worker/manifests/startserver.pp
@@ -24,10 +24,9 @@ class is_analytics_worker::startserver inherits is_analytics_worker::params {
   }
 
   # Start the service
-  service { $service_name:
+  service { $profile:
     enable    => true,
     ensure    => running,
-    subscribe => File["binary"],
+    subscribe => File["wso2-binary"],
   }
-
 }
