a7f7e5ae32accfd0208bd767c55a9340fee01baf
Savidude <mysticsavy@gmail.com>
2019-07-09 09:50:52 +0530

    Add capabilities for performance tuning

--- modules/is_common/manifests/init.pp
+++ modules/is_common/manifests/init.pp
@@ -41,6 +41,22 @@ class is_common inherits is_common::params {
   }
 
   /*
+  * System Performance Tuning
+  */
+  if $enable_performance_tuning {
+    $performance_tuning_flie_list.each | String $file | {
+      file { "/${file}":
+        path    => "/${file}",
+        ensure  => present,
+        recurse => remote,
+        source  => "puppet:///modules/${module_name}/system/${file}",
+        owner   => root,
+        group   => root
+      }
+    }
+  }
+
+  /*
   * Java Distribution
   */
 
